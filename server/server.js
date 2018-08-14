4dc3d5529da82df9ff39ea479a12a66f0fc7fbcc
jlstr <jlecampana@gmail.com>
2018-08-14 17:30:06 -0500

    Checkpoint: Moving CSS to /css

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3001;
 
 // Using MiddleWare
 app.use('/fonts', express.static('fonts'));
+app.use('/css', express.static('css'));
 
 app.get('/', (request, response) => {
   response.sendFile('index.html', { 'root': 'static' });
