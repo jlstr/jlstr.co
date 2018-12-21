4d88c4600f07ac5152f6399ae40776100b82aba7
jlstr <jlecampana@gmail.com>
2018-12-21 15:16:35 -0500

    Fix NavBar issue for Mobile / update Gulp (4.0)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', () => {
-  gulp.src('scss/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css/'));
-});
+// gulp.task('sass', () => {
+//   gulp.src('scss/**/*.scss')
+//       .pipe(sass().on('error', sass.logError))
+//       .pipe(gulp.dest('./css/'));
+// });
+
+
+// Gulp 4 Syntax
+
+const style = () => {
+  return (
+    gulp.src('scss/**/*.scss')
+      .pipe(sass())
+      .on('error', sass.logError)
+      .pipe(gulp.dest('./css/'))
+  );
+}
+
+exports.style = style;
