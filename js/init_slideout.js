e6dae1b748923322564ee6d57a468705ceb382d8
jlstr <jlecampana@gmail.com>
2019-02-12 16:01:06 -0500

    Checkpoint

---
+++ js/init_slideout.js
@@ -0,0 +1,14 @@
+$(() => {
+
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+
+  document.querySelector('.toggle-button').addEventListener('click', function() {
+    slideout.toggle();
+  });
+
+});
