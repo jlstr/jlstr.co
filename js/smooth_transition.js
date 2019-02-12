9d3959d90d3dff54e4a55eaa84500a2396cf22e0
jlstr <jlecampana@gmail.com>
2019-02-12 18:49:58 -0500

    Transition to Skills

---
+++ js/smooth_transition.js
@@ -0,0 +1,11 @@
+$(() => {
+  $('.scroll-to').click((e) => {
+    e.preventDefault();
+    var target = $(e.target);
+
+    $('html, body').animate({
+      scrollTop: $(target.attr('href')).offset().top
+    }, 500, 'linear');
+
+  });
+});
